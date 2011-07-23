eb918c433c075c94d48755632d582d1ed5c7abdc
Terry Li <terence.li@utoronto.ca>
2011-07-23 17:38:53 -0400

    version one

---
+++ SkeletalViewer.h
@@ -0,0 +1,82 @@
+/********************************************************************
+*                                                                       *
+*   SkeletalViewer.h -- Declares of CSkeletalViewerApp class            *
+*                                                                       *
+* Copyright (c) Microsoft Corp. All rights reserved.                    *
+*                                                                       *
+* This code is licensed under the terms of the                          *
+* Microsoft Kinect for Windows SDK (Beta) from Microsoft Research       *
+* License Agreement: http://research.microsoft.com/KinectSDK-ToU        *
+*                                                                       *
+************************************************************************/
+
+#pragma once
+
+#include "resource.h"
+#include "MSR_NuiApi.h"
+#include "DrawDevice.h"
+
+#define SZ_APPDLG_WINDOW_CLASS        _T("SkeletalViewerAppDlgWndClass")
+
+class CSkeletalViewerApp
+{
+public:
+    HRESULT Nui_Init();
+    void                    Nui_UnInit( );
+    void                    Nui_GotDepthAlert( );
+    void                    Nui_GotVideoAlert( );
+    void                    Nui_GotSkeletonAlert( );
+    void                    Nui_Zero();
+    void                    Nui_BlankSkeletonScreen( HWND hWnd );
+    void                    Nui_DoDoubleBuffer(HWND hWnd,HDC hDC);
+    void                    Nui_DrawSkeleton( bool bBlank, NUI_SKELETON_DATA * pSkel, HWND hWnd, int WhichSkeletonColor );
+    void                    Nui_DrawSkeletonSegment( NUI_SKELETON_DATA * pSkel, int numJoints, ... );
+
+    RGBQUAD                 Nui_ShortToQuad_Highlight( USHORT s );
+	RGBQUAD                 Nui_ShortToQuad_Depth( USHORT s );
+
+    static LONG CALLBACK    WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+
+    HWND m_hWnd;
+
+	USHORT *				initialdepth;
+	int						FrameCount;
+
+private:
+    static DWORD WINAPI     Nui_ProcessThread(LPVOID pParam);
+
+
+    // NUI and draw stuff
+    DrawDevice    m_DrawDepth;
+    DrawDevice    m_DrawVideo;
+
+    // thread handling
+    HANDLE        m_hThNuiProcess;
+    HANDLE        m_hEvNuiProcessStop;
+
+    HANDLE        m_hNextDepthFrameEvent;
+    HANDLE        m_hNextVideoFrameEvent;
+    HANDLE        m_hNextSkeletonEvent;
+    HANDLE        m_pDepthStreamHandle;
+    HANDLE        m_pVideoStreamHandle;
+    HFONT         m_hFontFPS;
+    HPEN          m_Pen[6];
+    HDC           m_SkeletonDC;
+    HBITMAP       m_SkeletonBMP;
+    HGDIOBJ       m_SkeletonOldObj;
+    int           m_PensTotal;
+    POINT         m_Points[NUI_SKELETON_POSITION_COUNT];
+    RGBQUAD       m_rgbWk[640*480];
+	ULONGLONG	  m_videoDelay;
+	UINT		  m_videoCache[640*480];
+	USHORT		  m_playerMap[640*480];
+    int           m_LastSkeletonFoundTime;
+    bool          m_bScreenBlanked;
+    int           m_FramesTotal;
+    int           m_LastFPStime;
+    int           m_LastFramesTotal;
+};
+
+int MessageBoxResource(HWND hwnd,UINT nID,UINT nType);
+
+
